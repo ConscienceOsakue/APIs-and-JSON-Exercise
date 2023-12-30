9dfc8b1b7c219187f703fa7f7c150df59c169322
Conscience Osakue <consciencenosa@gmail.com>
2023-12-30 16:53:16 -0600

    Initial commit

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,41 @@ using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        private HttpClient _client;
+
+        public RonVSKanyeAPI(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string Ron()
+        {
+            //Ron Swanson URL
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse);
+
+            return ronQuote[0].ToString();
+        }
+
+
+        public string Kanye()
+        {
+            //API Url
+            var kanyeURL = "https://api.kanye.rest";
+
+            //Stores the JSON repsonse in a variable
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            //Parses through the response we received to get the value associated with
+            //the NAME "quote"
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+       
     }
 }
