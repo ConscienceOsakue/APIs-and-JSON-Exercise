138d904854f6c9cf3007a060bc6603ecfe28bb80
Conscience Osakue <consciencenosa@gmail.com>
2023-12-30 20:12:34 -0600

    OpenWeatherMapAPI

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
+using System.Reflection.Emit;
 using System.Runtime.Intrinsics.X86;
 
 namespace APIsAndJSON
@@ -7,7 +8,8 @@ namespace APIsAndJSON
     {
         static void Main(string[] args)
         {
-            var client = new HttpClient();
+            #region RonVSKanye
+            /*var client = new HttpClient();
             var quote = new RonVSKanyeAPI(client);
 
             for (int i = 0; i < 5; i++)
@@ -17,8 +19,19 @@ namespace APIsAndJSON
 
                 Console.WriteLine($"Ron: {quote.Ron()}");
 
-            }
+            }*/
+            #endregion
 
+            String Key = File.ReadAllText("appsettings.json");
+            String APIKey = JObject.Parse(Key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Please enter your zipcode");
+            var zipCode = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode},&units=imperial&appid={APIKey}";
+
+            Console.WriteLine();
+            Console.WriteLine($"It is currently {OpenWeatherMapAPI.GetTemp(apiCall)} degrees F in your location.");
         }
 
 
